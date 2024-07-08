40f978557230addd30196d7c0d83eaa1af08aba8
jp378 <jpoutoa865@gmail.com>
2024-07-09 02:19:19 +1200

    Initial commit

---
+++ resolve.java
@@ -0,0 +1,7 @@
+public class resolve{
+    public static void main(String[] args){
+        if(args.length != 2){
+            System.out.println("Usage: resolve <name1> <name2> ... <nameN>");
+        }
+    }
+}
