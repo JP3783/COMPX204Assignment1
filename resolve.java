56888d45573eccbaa6d9ec258c44639011afa3e0
jp378 <jpoutoa865@gmail.com>
2024-07-19 12:59:16 +1200

    Latest commit

--- resolve.java
+++ resolve.java
@@ -3,5 +3,17 @@ public class resolve{
         if(args.length != 2){
             System.out.println("Usage: resolve <name1> <name2> ... <nameN>");
         }
+
+        Boolean found = true;
+        String notFoundTextMessage = "unknown host";
+
+
+
+        if(found == true){
+
+        } else{
+            found = false;
+            System.out.println(notFoundTextMessage);
+        }
     }
 }
