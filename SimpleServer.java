2c3f79eb3fbd3c42dd7ee09532fbea3c2b8cadcf
jp378 <jpoutoa865@gmail.com>
2024-07-22 15:16:38 +1200

    added a main method to the SimpleServer

--- SimpleServer.java
+++ SimpleServer.java
@@ -1,5 +1,10 @@
 import java.net.*;
 
 public class SimpleServer {
-    
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: <> <>");
+            return;
+        }
+    }
 }
