56316db0f9332b0a49178dc44d79da852475b4db
jp378 <jpoutoa865@gmail.com>
2024-07-23 15:40:01 +1200

    Made some adjustments to SimpleServer

--- SimpleServer.java
+++ SimpleServer.java
@@ -1,10 +1,22 @@
+import java.io.IOException;
 import java.net.*;
 
 public class SimpleServer {
-    public static void main(String[] args) {
-        if (args.length != 2) {
-            System.out.println("Usage: <> <>");
-            return;
-        }
+    public static void main(String[] args) throws IOException {
+        //Get the port which it is listening to and display to output
+        ServerSocket serverSocket = new ServerSocket();
+        System.out.println("Listening on port " + serverSocket.getLocalPort());
+
+        
+        //Close the connection
+        serverSocket.close();
+    }
+
+    /**
+     * Binds the ServerSocket to a specific address
+     * @param endpoint the IP address and port number to bind to
+     */
+    public void bind(SocketAddress endpoint){
+
     }
 }
